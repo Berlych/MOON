bab31fa31495fb025686f96e4c8259ea392c2150
Berlych <113550665+Berlych@users.noreply.github.com>
2022-09-14 13:56:41 -0500

    Add files via upload

---
+++ Churata/Berly.py
@@ -0,0 +1,38 @@
+"""ok=int(input("ingrese el primer número"))
+ey=int(input("ingrese el segundo número"))
+suma=(ok)+(ey)
+print(suma)"""
+"""lista=[1,4,3,6,5]
+print(lista)
+lista.append("welcome")
+print(lista)
+lista.pop(-1)
+print(lista)
+lista.sort()
+print(lista)
+lista.reverse()
+print(lista)
+lista.remove(1)
+print(lista)"""
+"""lista=[1,1,1,2,3,4,5,5,6]
+print(lista)
+tupla=(1,1,1,1,2,3,3,3,3,3,4,5,6)
+print(tupla)
+print((tupla.count(3)))
+tup=tuple(lista)
+print(tup)"""
+"""diccionario = {"gato":"cat","perro":"dog","cancho":"pig"}
+print(diccionario["gato"])"""
+"""edad=0
+if edad>18:
+     print("maryor de edad")
+elif edad<=0:
+    print("tu edad no puede ser 0")
+else:
+    print("menor de edad")"""
+
+print("comienzo")
+for i in[1,2,3,4]:
+    print("hola", end="/")
+print()
+print("Final")
